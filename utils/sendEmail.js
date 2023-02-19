0f8bc5bc16f503fee2e8cd22494b4aa4107b8a92
Dennis <okoyedennis7@gmail.com>
2023-02-19 10:39:10 +0100

    initial commit

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,10 +1,24 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
-  // Create Email Transporter
+  // Create Email Transporter For Outlook
+  // const transporter = nodemailer.createTransport({
+  //   host: process.env.EMAIL_HOST,
+  //   port: 587,
+  //   auth: {
+  //     user: process.env.EMAIL_USER,
+  //     pass: process.env.EMAIL_PASS,
+  //   },
+  //   tls: {
+  //     rejectUnauthorized: false,
+  //   },
+  // });
+
+  // Create Email Transporter For Gmail
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: 587,
+    port: 465,
+    seture: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -28,7 +42,7 @@ const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
     if (err) {
       console.log(err);
     } else {
-      console.log(info);
+      console.log("Email sent " + info);
     }
   });
 };
